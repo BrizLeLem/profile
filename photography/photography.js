c036c81a49cf23cbe739c38890a1283e25b1255d
Briz Le Lem <briz.lelem@gmail.com>
2021-01-04 18:03:41 +0100

    Added Dark Mode to all pages

--- photography/photography.js
+++ photography/photography.js
@@ -1,19 +1,61 @@
+// Dark Mode
+  // Event Handlers
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Store user pref for future visits
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+  // check for saved user preferences
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Navbar OnScroll
+
 var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
+    if(document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        $("#header").css("background","var(--bkg-color)");
+        $("#header").css("color","var(--text-color)");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
         $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+            $(this).css("border-bottom","2px solid rgb(40, 114, 113)");
         },function(){
             $(this).css("border-bottom","2px solid transparent");
         });
     }else{
         $("#header").css("background","transparent");
-        $("#header").css("color","#000");
+        $("#header").css("color","#fff");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
         $("#header").css("padding","6vh 4vw");
         $("#navigation a").hover(function(){
@@ -24,6 +66,8 @@ if ((width >= 1000)){
     }
 }
 }
+
+ // Magnify for showing pictures
 
 function magnify(imglink){
     $("#img_here").css("background",`url('${imglink}') center center`);
@@ -43,6 +87,8 @@ function closemagnify(){
     },800);
 }
 
+// Loading animation
+
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
@@ -50,6 +96,8 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1650);
+
+//
 
 $(document).ready(function(){
     $("a").on('click', function(event) {
@@ -64,3 +112,4 @@ $(document).ready(function(){
        }
       });
   });
+
