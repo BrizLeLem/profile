e32c4902f2fbbfc65bc71018354bac1a9e1d9bdb
Briz Le Lem <briz.lelem@gmail.com>
2021-01-04 17:29:16 +0100

    Added dark mode for homepage

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+// Dark Mode
+  // Event Handlers
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Store user pref for future visits
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+  // check for saved user preferences
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Navbar OnScroll
+
 var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
+        $("#header").css("background","var(--bkg-color)");
+        $("#header").css("color","var(--text-color)");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
         $("#navigation a").hover(function(){
@@ -25,6 +67,8 @@ if ((width >= 1000)){
 }
 }
 
+ // Magnify for showing pictures
+
 function magnify(imglink){
     $("#img_here").css("background",`url('${imglink}') center center`);
     $("#magnify").css("display","flex");
@@ -43,6 +87,8 @@ function closemagnify(){
     },800);
 }
 
+// Loading animation
+
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
@@ -50,6 +96,8 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1650);
+
+//
 
 $(document).ready(function(){
     $("a").on('click', function(event) {
@@ -64,3 +112,4 @@ $(document).ready(function(){
        }
       });
   });
+
