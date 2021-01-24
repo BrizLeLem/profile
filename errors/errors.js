38b69ba4679f3ebaab67e81ebe4a31c036a29213
Briz Le Lem <briz.lelem@gmail.com>
2021-01-24 19:21:58 +0100

    customized 404 & 500 error pages

---
+++ errors/errors.js
@@ -0,0 +1,62 @@
+// Navbar OnScroll
+
+var width = $(window).width();
+window.onscroll = function(){
+if ((width >= 1000)){
+    if(document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        $("#header").css("background","var(--bkg-color)");
+        $("#header").css("color","var(--text-color)");
+        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+        $("#header").css("padding","4vh 4vw");
+        $("#navigation a").hover(function(){
+            $(this).css("border-bottom","2px solid rgb(40, 114, 113)");
+        },function(){
+            $(this).css("border-bottom","2px solid transparent");
+        });
+    }else{
+        $("#header").css("background","transparent");
+        $("#header").css("color","#fff");
+        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+        $("#header").css("padding","6vh 4vw");
+        $("#navigation a").hover(function(){
+            $(this).css("border-bottom","2px solid #fff");
+        },function(){
+            $(this).css("border-bottom","2px solid transparent");
+        });
+    }
+}
+}
+
+// BKGND PICTURE ANIMATION
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function animate() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('#error-image').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(animate);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+animate();
